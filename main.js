0eff683d36fd2d53a24fecdfeb8fa6a902312542
Damian Cur <damian.cur91@gmail.com>
2021-03-14 18:38:40 +0100

    added init function

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 let size = 10;
 let orderElement = 1;
 
+
+
+
 const init = () => {
- // tutaj kod
+    const btn = document.createElement("button");
+    document.body.appendChild(btn);
+    const ul = document.createElement("ul");
+    document.body.appendChild(ul);
+
+    btn.textContent = "Dodaj 10 elementów listy";
+    btn.addEventListener("click", createLiElements);
 }
 
 const createLiElements = () => {
- // tutaj kod
+
 }
 
 init()
