3cabdf3db4ed076351a4856365648f0491ce2477
Damian Cur <damian.cur91@gmail.com>
2021-03-14 19:33:14 +0100

    added "li+10+1 fontSize" feature

--- main.js
+++ main.js
@@ -8,13 +8,30 @@ const init = () => {
     const btn = document.createElement("button");
     document.body.appendChild(btn);
     const ul = document.createElement("ul");
+    ul.style.listStyle = "none";
     document.body.appendChild(ul);
 
     btn.textContent = "Dodaj 10 elementów listy";
     btn.addEventListener("click", createLiElements);
 }
 
+
+
 const createLiElements = () => {
+
+
+
+    for (let i = 0; i < 10; i++) {
+        const li = document.createElement("li");
+        li.textContent = `Element nr ${orderElement++}`
+        document.querySelector("ul").appendChild(li);
+
+        li.style.fontSize = `${size++}px`;
+
+
+    }
+
+
 
 }
 
